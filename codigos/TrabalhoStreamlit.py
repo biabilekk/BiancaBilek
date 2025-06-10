7b6344652ae9443b353efb066337e7fb56de9c0e
biabilekk <bianca.bilek@aluno.faculdadeimpacta.com.br>
2025-06-10 18:45:24 -0300

    Update TrabalhoStreamlit.py

--- codigos/TrabalhoStreamlit.py
+++ codigos/TrabalhoStreamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('dadostratadosfarfetch.csv', sep=';')
+df = pd.read_csv('codigos/dadostratadosfarfetch.csv', sep=';')
 
 st.dataframe(df)
 
